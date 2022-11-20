bf4a058089fe1ed7d47e7dae296578b63de084dc
alexkrewson <alex.krewson@gmail.com>
2022-11-20 15:09:37 -0800

    favicon and 'LLC'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import '../src/sassystyle.scss';
+import machine from './machine.jpeg';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="centered">
+        <div className="upper">
+          <h1>Flow Rate Carpet Cleaning LLC</h1>
+
+        </div>
+
+        <div className="lower">
+          <div className="lower-left">
+            <img src={machine} alt="" />
+          </div>
+          <div className="lower-right">
+            <ul>
+              <li>$35 per room (up to 250 square feet each)</li>
+              <li>furniture moving</li>
+              <li>dry vacuuming</li>
+              <li>stain pre-treament</li>
+              <li>industry leading hot water extraction (steam cleaning)</li>
+              <li>deoderization</li>
+              <li>dries same day</li>
+              <li>mobile equipment capable of reaching all homes and high rises</li>
+              <li>upholstery cleaning</li>
+              <li>curtain cleaning</li>
+              <li>use of biodegradable Dr Bronners Sal Suds unless otherwise desired</li>
+              <li>info@flowratecleaning.com</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
